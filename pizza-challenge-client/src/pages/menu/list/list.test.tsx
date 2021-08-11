207dd6ffd3a40b99b8c441cc3b31db5eb14292d5
Osama Rezk <osama.rezk@docusign.com>
2021-08-12 00:00:31 +0200

    update menu list test

--- pizza-challenge-client/src/pages/menu/list/list.test.tsx
+++ pizza-challenge-client/src/pages/menu/list/list.test.tsx
@@ -9,12 +9,16 @@ let pizzas = [
     price: 15,
     description: "A classic pizza with our special sauce.",
     images: [],
+    addons: [],
+    sizes: [],
   },
 ];
 
-describe("testing Menu List  Component ", () => {
-  it("It should Render 1  Item in the  List", () => {
-    render(<List items={pizzas} />);
+describe("testing Menu List  Component", () => {
+  it("Should Render 1  Item in the List", () => {
+    const mockOnClick = jest.fn();
+
+    render(<List items={pizzas} onItemClick={mockOnClick} />);
 
     const startsWrapperChildrenCount =
       screen.getByTestId("pizza-list").children.length;
