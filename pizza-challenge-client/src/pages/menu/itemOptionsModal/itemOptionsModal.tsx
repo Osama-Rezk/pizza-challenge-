6fcdb2191f1953e46198ffc0678562417654c8a5
Osama Rezk <osama.rezk@docusign.com>
2021-08-11 21:53:10 +0200

    fix item options modal

--- pizza-challenge-client/src/pages/menu/itemOptionsModal/itemOptionsModal.tsx
+++ pizza-challenge-client/src/pages/menu/itemOptionsModal/itemOptionsModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import {
   Modal,
@@ -42,6 +42,13 @@ export const ItemOptionsModal = (props: ItemOptionsModalProps) => {
   const canCheckout = !!(size.name && selectedAddons.length);
 
   const history = useHistory();
+
+  useEffect(() => {
+    if (!isOpen) {
+      setSize({} as Size);
+      setSelectedAddons([]);
+    }
+  }, [isOpen]);
 
   const renderSizes = () => {
     return sizes.map(({ name, price }: Size, index) => (
